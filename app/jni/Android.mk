2fb46280c4d3d35c4b4fe5c3237f0f2a33f5b5fc
Zash60 <xath06@gmail.com>
2025-12-01 01:12:52 -0300

    Update Android.mk

--- app/jni/Android.mk
+++ app/jni/Android.mk
@@ -1,25 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := emu
-LOCAL_SRC_FILES := common/emulator.cpp
-LOCAL_C_INCLUDES += $(LOCAL_PATH)/..
-LOCAL_CFLAGS += -fPIE
-LOCAL_LDFLAGS += -fPIE -pie
-include $(BUILD_SHARED_LIBRARY)
+# Include common module
+include $(LOCAL_PATH)/common/Android.mk
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := nes
-LOCAL_SRC_FILES := nes/NES.cpp nes/Mapper.cpp nes/PPU.cpp nes/APU.cpp nes/MMC3.cpp nes/NsfPlayer.cpp nes/Vrc6.cpp
-LOCAL_C_INCLUDES += $(LOCAL_PATH)/nes
-LOCAL_CFLAGS += -fPIE
-LOCAL_LDFLAGS += -fPIE -pie
-include $(BUILD_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := fceumm
-LOCAL_SRC_FILES := fceu/mappers/UNIF.cpp fceu/mappers/UNIF_CIs.cpp fceu/mappers/UNIF_8001.cpp fceu/mappers/UNIF_B...
-LOCAL_C_INCLUDES += $(LOCAL_PATH)/fceu
-LOCAL_CFLAGS += -fPIE
-LOCAL_LDFLAGS += -fPIE -pie
-include $(BUILD_SHARED_LIBRARY)
+# Include NES library module  
+include $(LOCAL_PATH)/neslib/Android.mk
